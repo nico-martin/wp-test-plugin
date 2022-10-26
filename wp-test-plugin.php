31964e8d09240dad7868399d24f545020da11f35
Nico Martin <mail@nico.dev>
2022-10-26 20:05:18 +0200

    init

---
+++ wp-test-plugin.php
@@ -0,0 +1,13 @@
+
+<?php
+
+/*
+Plugin Name: Nico WP Test Plugin
+Plugin URI: https://github.com/nico-martin/wp-test-plugin
+Description: -
+Author: Nico Martin - mail@nico.dev
+Author URI: https://nico.dev
+Version: 0.0.1
+Text Domain: nwptp
+Domain Path: /languages
+*/
