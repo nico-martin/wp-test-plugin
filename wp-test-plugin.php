c697ae2d5176d5b72708ea9ca8b0442c5283ebdd
Nico Martin <mail@nico.dev>
2022-11-03 11:35:03 +0100

    Update wp-test-plugin.php

--- wp-test-plugin.php
+++ wp-test-plugin.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/nico-martin/wp-test-plugin
 Description: -
 Author: Nico Martin - mail@nico.dev
 Author URI: https://nico.dev
-Version: 0.1.1
+Version: 0.1.2
 Text Domain: nwptp
 Domain Path: /languages
 */
